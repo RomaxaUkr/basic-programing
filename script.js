30996040d47720c766fb681b86328d265026488a
RomaxaUkr <roman.pokydanets@gmail.com>
2020-07-22 11:23:16 +0400

    add lesson 1

---
+++ script.js
@@ -0,0 +1,16 @@
+let city;
+city="Kyiv";
+city="Lviv";
+let adress=city;
+alert(adress);
+
+let n=4;
+let result=Math.pow(n,3);
+console.log(result);
+
+let pen=5.25;
+let pquant=4;
+let pencil=3.5;
+let pencquant=6;
+let result2=pen*pquant+pencil*pencquant;
+document.write(`Загальна вартість покупки склала ${result2} грн`);
